3d0b6cf7612b326b3cbf462d59b0cd2922da8c6e
LENOVO <22371554@buaa.edu.cn>
2024-09-06 23:41:20 +0800

    cross

--- cross.py
+++ cross.py
@@ -1,5 +1,7 @@
 import numpy as np
+from astropy.modeling.functional_models import Box1D
 from mpmath.math2 import sqrt2
+from statsmodels.tsa.statespace.simulation_smoother import check_random_state
 
 # 2 dot -> 4 edge
 lambda_of_head = 27.5 / 286
@@ -23,11 +25,16 @@ def cal_long_side(front_dot, back_dot, head):
     # print(front_end + move_vector, back_end + move_vector, front_end - move_vector, back_end - move_vector)
     return front_end + move_vector, back_end + move_vector, front_end - move_vector, back_end - move_vector
 
-cal_long_side(np.array([0,0]), np.array([165 * sqrt2 / 2, 165 * sqrt2 / 2]), False)
+# cal_long_side(np.array([0,0]), np.array([165 * sqrt2 / 2, 165 * sqrt2 / 2]), False)
 
 
 # return true if 2 edges cross
-def cross_test(A0, A1, B0, B1):
+def cross_test(edge1, edge2):
+    A0 = edge1[0]
+    A1 = edge1[1]
+    B0 = edge2[0]
+    B1 = edge2[1]
+
     A0B0 = B0 - A0
     A0B1 = B1 - A0
     A0A1 = A1 - A0
@@ -42,8 +49,40 @@ def cross_test(A0, A1, B0, B1):
     vector3 = np.cross(B0B1, B0A0)
     vector4 = np.cross(B0B1, B0A1)
 
-    # print(vector1, vector2)
-    # print(vector3, vector4)
+    #print(vector1, vector2)
+    #print(vector3, vector4)
     return vector1 * vector2 <= 0 and vector3 * vector4 <= 0
 
+#print(cross_test(np.array([[0,0],[1,1]]),np.array([[0,-1],[-1,3]])))
 
+def all_cross_check(dot_matrix):
+    edge_list = []
+    for i in range(dot_matrix.shape[0] - 1):
+        A0, A1, B0, B1 = cal_long_side(dot_matrix[i], dot_matrix[i + 1], i == 0)
+        edge_list.append(np.array([A0, A1])) # even：inside
+        edge_list.append(np.array([B0, B1])) # odd: outside
+
+    cross = False
+    for i in range(2, len(edge_list), 2):
+        for j in range(i - 3, 0, -2):
+            if cross_test(edge_list[i], edge_list[j]):
+                cross = True
+                break
+
+    return cross
+
+print(all_cross_check(np.array([[0,0],[386,0],[386 + 165, 0]])))
+
+from openpyxl import load_workbook
+
+# 加载现有的Excel文件
+workbook = load_workbook('example.xlsx')
+# 选择工作表
+sheet = workbook.active
+# 读取单元格内容
+cell = sheet['A1']
+print(cell.value)
+# 写入单元格内容
+sheet['A2'] = 'Hello, World!'
+# 保存对Excel文件的更改
+workbook.save('example.xlsx')

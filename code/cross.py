4bfa8bd18fb21e2a07543545f74fbd5e6fb17c09
LENOVO <22371554@buaa.edu.cn>
2024-09-07 17:19:40 +0800

    pso algrithm

--- code/cross.py
+++ code/cross.py
@@ -1,13 +1,18 @@
+import random
+
 import numpy as np
 from astropy.modeling.functional_models import Box1D
+from joblib.testing import param
 from mpmath.math2 import sqrt2
 from numpy.matrixlib.defmatrix import matrix
+from pandas.core.indexes.multi import sparsify_labels
 from statsmodels.tsa.statespace.simulation_smoother import check_random_state
 from openpyxl import load_workbook
 from sympy.abc import epsilon
 
 import config
 from calculate import t_to_dis
+from calculate import t_to_theta
 
 # 2 dot -> 4 edge
 lambda_of_head = 27.5 / 286
@@ -76,6 +81,7 @@ def all_cross_check(dot_matrix):
         for j in range(i - 3, 0, -2):
             #print(i, j)
             if cross_test(edge_list[i], edge_list[j]):
+                print(i // 2, j // 2)
                 cross = True
                 break
         if cross:
@@ -84,52 +90,56 @@ def all_cross_check(dot_matrix):
     return cross
 
 #print(all_cross_check(np.array([[0,0],[386,0],[386 + 165, 0]])))
+#
+# dot_matrix = np.empty((300, 224, 2))
+# def read_from_dis_excel(time):
+#      # 从表格读取，后面改成从程序读取
+#      file_path = '../result1_dis.xlsx'
+#      wb = load_workbook(file_path)
+#
+#      # 选择工作表
+#      ws = wb.active  # 或者 wb['SheetName']
+#      rows_to_read = range(2, 450)
+#      cols_to_read = range(time + 2, time + 3)
+#
+#      # 如果你需要行号和列号，可以这样做：
+#      for row_num in rows_to_read:
+#          for col_num in  cols_to_read:
+#              cell_value = ws.cell(row=row_num, column=col_num).value
+#              #print(f'Cell ({row_num}, {col_num}): {cell_value}')
+#              dot_matrix[time][row_num // 2 - 1][row_num % 2] = cell_value
+#
+#      #print(dot_matrix)
 
-dot_matrix = np.empty((300, 224, 2))
-def read_from_dis_excel(time):
-     # 从表格读取，后面改成从程序读取
-     file_path = '../result1_dis.xlsx'
-     wb = load_workbook(file_path)
+# def check_excel():
+#      max_time = 300
+#      dot_matrix = np.empty((max_time + 1, 224, 2))
+#      for i in range(290, max_time + 1):
+#          print(f'{i}s')
+#          read_from_dis_excel(i)
+#          print(all_cross_check(dot_matrix[i]))
 
-     # 选择工作表
-     ws = wb.active  # 或者 wb['SheetName']
-     rows_to_read = range(2, 450)
-     cols_to_read = range(time + 2, time + 3)
+def cal_nearest(space):
+    #print(t_to_dis(0, space))
+    delta = 10
+    crash = 280
+    epsilon = 0.01
+    while delta >= epsilon:
+        low = 230
+        high = crash
+        time = low
+        while time < high:
+            time = time + delta
+            matrix = t_to_dis(time, space)
+            print(time)
+            #print(all_cross_check(matrix))
+            if all_cross_check(matrix):
+                crash = time if time < crash else crash
+                break
+        delta /= 10
+    print(crash)
 
-     # 如果你需要行号和列号，可以这样做：
-     for row_num in rows_to_read:
-         for col_num in  cols_to_read:
-             cell_value = ws.cell(row=row_num, column=col_num).value
-             #print(f'Cell ({row_num}, {col_num}): {cell_value}')
-             dot_matrix[time][row_num // 2 - 1][row_num % 2] = cell_value
-
-     #print(dot_matrix)
-
-def check_excel():
-     max_time = 300
-     dot_matrix = np.empty((max_time + 1, 224, 2))
-     for i in range(290, max_time + 1):
-         print(f'{i}s')
-         read_from_dis_excel(i)
-         print(all_cross_check(dot_matrix[i]))
-
-delta = 0.1
-crash = 410
-epsilon = 0.01
-while delta >= epsilon:
-    low = 408
-    high = crash
-    time = low
-    while time < high:
-        time = time + delta
-        space = config.space  # 自行设置
-        matrix = t_to_dis(time, space)
-        print(time)
-        print(all_cross_check(matrix))
-        if all_cross_check(matrix):
-            crash = time if time < crash else crash
-            break
-    delta /= 10
+#cal_nearest(45)
 
 #
 # for t in range(41240, 41260, 1):
@@ -138,3 +148,49 @@ while delta >= epsilon:
 #     #print(t_to_dis(time))
 #     print(time)
 #     print(all_cross_check(matrix))
+
+max = 30
+space = 45.5
+w = 0.5
+c1 = 0.5
+c2 = 1
+max_time = 350
+def pso(n, c1, c2):
+    partical = []
+    partical_v = []
+    pbest    = []
+    gbest    = (max_time, True)
+    for i in range(n):
+        print(i)
+        partical.append(random.random() * max_time)
+        partical_v.append(random.random())
+        pbest.append((partical[i], all_cross_check(t_to_dis(partical[i], space))))
+        if pbest[i][0] < gbest[0] and pbest[i][1]:
+            gbest = pbest[i]
+
+    for j in range(max):
+        for i in range(n):
+            partical_v[i] = w * partical_v[i] + c1 * random.random() * (
+                        pbest[i][0] - partical[i]) + c2 * random.random() * (gbest[0] - partical[i])
+            partical[i] = partical[i] + partical_v[i]
+
+            if partical[i] > 500:
+                partical[i] = 500
+            if partical[i] < 0:
+                partical[i] = 0
+
+            cross = all_cross_check(t_to_dis(partical[i], space))
+            if not pbest[i][1] and cross:
+                pbest[i] = (partical[i], True)
+
+            elif pbest[i][1] and cross:
+                pbest[i] = pbest[i] if pbest[i][0] < partical[i] else (partical[i], cross)
+
+            if pbest[i][0] < gbest[0] and pbest[i][1]:
+                gbest = pbest[i]
+
+            print(f'{i}, {pbest[i]}, {gbest}')
+            print(f'{t_to_theta(gbest[0]) / 2 / np.pi * space}')
+
+
+pso(10, c1, c2)
